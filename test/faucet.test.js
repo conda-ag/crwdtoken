a57edd29b6d2832feac8a3d4b76bd2c922dabc2f
Johannes Mayer Conda <johannes.mayer@conda.at>
2019-07-26 12:46:24 +0200

    TestnetFaucet added with tests

---
+++ test/faucet.test.js
@@ -0,0 +1,94 @@
+import { reverting } from "./helpers/compare.js";
+import { deployTokenJustLikeInMigrations } from "./helpers/deployTokenHelper.js";
+
+import { BigNumber } from "./helpers/customBN.js";
+
+const TestnetFaucet = artifacts.require("./TestnetFaucet.sol");
+const CrwdToken = artifacts.require("./CrwdToken.sol");
+
+import {
+  advanceBlock,
+  advanceToBlock,
+  increaseTime,
+  increaseTimeTo,
+  duration,
+  revert,
+  latestTime
+} from "truffle-test-helpers";
+
+const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+
+require("chai")
+  .use(require("chai-as-promised"))
+  .use(require("chai-bn")(BigNumber))
+  .should();
+
+contract("Faucet on Testnet", accounts => {
+  let testnetFaucet = null;
+  let token = null;
+
+  let stateControl = null;
+  let owner = null;
+  let unknown = null;
+
+  beforeEach(async () => {
+    owner = accounts[0];
+    stateControl = accounts[0];
+    unknown = accounts[9];
+
+    testnetFaucet = await TestnetFaucet.new();
+
+    token = await CrwdToken.new(
+      stateControl, //state control
+      testnetFaucet.address,
+      testnetFaucet.address,
+      testnetFaucet.address,
+      testnetFaucet.address, //15%
+      testnetFaucet.address, //15%
+      testnetFaucet.address, //10%
+      testnetFaucet.address //10%
+    );
+
+    await testnetFaucet.setTokenAddress(token.address);
+
+    //start ico
+    await token.updateEthICOThresholds(
+      "0",
+      (2 ^ 128).toString(),
+      "0",
+      "9999999999"
+    );
+  });
+
+  it("Receive CRWD via faucet", async () => {
+    await testnetFaucet.mint("123", { from: unknown });
+    assert.equal((await token.balanceOf(unknown)).toString(), "123");
+  });
+
+  it("Faucet suicide disables faucet", async () => {
+    await testnetFaucet.suicide();
+    await testnetFaucet
+      .mint("123", { from: unknown })
+      .should.be.rejectedWith(revert);
+    assert.equal((await token.balanceOf(unknown)).toString(), "0");
+  });
+
+  it("Faucet suicide only by owner", async () => {
+    await testnetFaucet
+      .suicide({ from: unknown })
+      .should.be.rejectedWith(revert);
+    assert.equal((await token.balanceOf(unknown)).toString(), "0");
+  });
+
+  it("setTokenAddress() by owner", async () => {
+    await testnetFaucet.setTokenAddress(ZERO_ADDRESS, { from: owner });
+    assert.equal((await testnetFaucet.faucetFor()).toString(), ZERO_ADDRESS);
+  });
+
+  it("setTokenAddress() by unknown should fail", async () => {
+    await testnetFaucet
+      .setTokenAddress(ZERO_ADDRESS, { from: unknown })
+      .should.be.rejectedWith(revert);
+    assert.equal((await testnetFaucet.faucetFor()).toString(), token.address);
+  });
+});
