4a6a8585f84450934c84b0fe49c0a7d6d79429ae
Johannes Mayer Conda <johannes.mayer@conda.at>
2019-08-21 15:40:16 +0200

    also flatten TestnetFaucet

--- scripts/flatten_contracts.sh
+++ scripts/flatten_contracts.sh
@@ -1,2 +1,3 @@
 pushd "./custom-truffle-flattener"
 node ./index.js "../contracts/CrwdToken.sol" > "../build/CrwdToken_flat.sol"
+node ./index.js "../contracts/TestnetFaucet.sol" > "../build/TestnetFaucet_flat.sol"
